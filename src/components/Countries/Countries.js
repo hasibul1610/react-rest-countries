74769209ad8c249bccb0749f7a09588bf1034429
hasibul1610 <bcse.hasibul@gmail.com>
2021-09-20 16:34:42 +0600

    updated

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const Countries = () => {
-    return (
-        <div>
-            <h2>This is from Countries in its own file</h2>
-        </div>
+const Countries=() =>{
+  const [countries, setCountries] = useState([]);
+  useEffect( ()=>{
+    fetch(' https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  },[])
+
+
+
+  return(
+    <div>
+      <h2>Travelling Around the World!!!!</h2>
+      <h2>Total Countries: {countries.length}</h2>
+      {
+        countries.map(country => <Country name={country.name} capital={country.capital}></Country>)
+      }
+
+    </div>
+  );
+}
+
+const Country=(props)=>{
+    return(
+      <div>
+        <h2>Name: {props.name}</h2>
+        <p>Capital: {props.capital}</p>
+      </div>
     );
-};
+}
 
 export default Countries;
